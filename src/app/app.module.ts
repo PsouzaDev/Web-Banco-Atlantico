45b9508326a00b87454b1a7fae2ad37710ec9b14
Pedro Souza <pedrosouza.developer@gmail.com>
2021-02-20 23:50:30 -0300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CaixasComponent } from './Components/caixas/caixas.component';
+import { SaqueComponent } from './Components/saque/saque.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaixasComponent,
+    SaqueComponent
   ],
   imports: [
     BrowserModule,
